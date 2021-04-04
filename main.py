f54c41b3519c7bb804a9cb85a27ed80932ca53c9
KungPaoChick <matthewriel63@gmail.com>
2021-04-04 15:21:34 +0000

    Added main files

---
+++ main.py
@@ -0,0 +1,79 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import WebDriverException
+import webdriver_conf
+import argparse
+import colorama
+import csv
+
+
+class Connection:
+
+    def __init__(self, browser_driver, search_query):
+        self.browser_driver = browser_driver
+        for query in search_query:
+            self.query = query
+
+    def conn(self):
+        url = 'https://youtube.com/results?search_query='
+        if self.browser_driver.get(url + '+'.join(self.query.split())) == None:
+            Get_Links(self.browser_driver, url + '+'.join(self.query.split())).scrape()
+
+
+class Get_Links:
+
+    def __init__(self, driver, link):
+        self.driver = driver
+        self.link = link
+
+    def scrape(self):
+        self.driver.get(self.link)
+        try:
+            with open('search_results.csv', 'w', encoding='utf-8') as f:
+                headers = ['Title', 'URL/Link']
+                writer = csv.writer(f, dialect='excel')
+                writer.writerow(headers)
+
+                container = WebDriverWait(self.driver, 0).until(
+                    EC.presence_of_element_located(
+                        (By.XPATH, '//*[@id="contents"]/ytd-item-section-renderer'))
+                )
+
+                length = []
+                for link in container.find_elements_by_xpath('//*[@id="video-title"]'):
+                    writer.writerows([[link.text, link.get_attribute('href')]])
+                    length.append(link.text)
+                    print(colorama.Fore.YELLOW,f'[!] {link.text}',
+                            colorama.Style.RESET_ALL, link.get_attribute('href'))
+
+                print(colorama.Fore.GREEN,
+                        f'[*] {len(length)} results.', colorama.Style.RESET_ALL)
+        except WebDriverException as err:
+            print(colorama.Fore.RED,
+              '[!!] WebDriver Failed To Function!', err, colorama.Style.RESET_ALL)
+            Get_Links(self.driver, self.link).scrape()
+        finally:
+            self.driver.quit()
+
+
+if __name__ == '__main__':
+    colorama.init()
+    parser = argparse.ArgumentParser(description='Scrapes links of top related videos.')
+
+    parser.add_argument('-s', '--search',
+                        nargs=1, metavar='SEARCH',
+                        action='store',
+                        help='Searches for top related videos based on input.')
+
+    args = parser.parse_args()
+    if args.search:
+        try:
+            options = webdriver_conf.get_driver_options('Chrome')
+            webdriver_conf.get_all_options('Chrome', options)
+            browser = webdriver_conf.get_driver('Chrome', options)
+
+            Connection(browser, [x for x in args.search]).conn()
+        except WebDriverException as err:
+            print(colorama.Fore.RED,
+                f'No WebDriver Found For Chrome!', err, colorama.Style.RESET_ALL)
---
+++ webdriver_conf.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+import time, colorama, os, platform
+
+
+# This function searches for your installed WebDriver
+def seek_driver(opsys, brs):
+    cwd = os.getcwd()
+    drivers = {'Chrome': 'chromedriver', 'Firefox': 'geckodriver'}
+    # windows
+    if opsys == 'Windows':
+        for root, dirs, files in os.walk(cwd):
+            dirs = dirs
+            if drivers[brs] + '.exe' in files:
+                return os.path.join(root, drivers[brs] + '.exe')
+
+    # macos and linux
+    elif opsys == 'Darwin' or opsys == 'Linux':
+        for root, dirs, files in os.walk(cwd):
+            if drivers[brs] in files:
+                return os.path.join(root, drivers[brs])
+
+
+# This function identifies your OS and proceeds to the seek_driver() function
+def identify_os(brs):
+    return seek_driver(platform.system(), brs)
+
+
+def log_finder(driver_browser):
+    log_files = {
+        'Chrome': 'chromedriver.log',
+        'Firefox': 'geckodriver.log'
+    }
+    for root, dirs, files in os.walk(os.getcwd()):
+        dirs = dirs
+        if log_files[driver_browser] in files:
+            return os.path.join(root, log_files[driver_browser])
+        else:
+            if not os.path.exists('logs/'):
+                os.makedirs('logs/')
+            with open(f'logs/{log_files[driver_browser]}', 'a') as f:
+                f.close
+                pass
+
+
+def get_driver_options(browser):
+    driver_options = {
+        'Chrome': webdriver.ChromeOptions,
+        'Firefox': webdriver.FirefoxOptions
+    }
+    return driver_options[browser]()
+
+
+def get_all_options(browser, options):
+    if browser == 'Chrome' or browser == 'Firefox':
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--incognito')
+        options.add_argument('--headless')
+
+
+def get_driver(select_browser, options):
+    colorama.init()
+    webdriver_browsers = {
+        'Chrome': webdriver.Chrome,
+        'Firefox': webdriver.Firefox
+    }
+    return webdriver_browsers[select_browser](
+        service_log_path=log_finder(select_browser),
+        executable_path=identify_os(select_browser),
+        options=options
+    )
